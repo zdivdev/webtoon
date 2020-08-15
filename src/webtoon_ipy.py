edb97d38e9ff18994054d78d2b9e9c881a2d4807
zdivdev <48123528+zdivdev@users.noreply.github.com>
2020-08-15 23:03:05 +0900

    WToon add

--- src/webtoon_ipy.py
+++ src/webtoon_ipy.py
@@ -29,9 +29,9 @@ def DebugPrint(data):
 
 def getWolfCom(comicsUrl,baseUrl,baseDir):
     doc = NSoup.NSoupClient.Connect(comicsUrl).Get()
-    DebugSave("a.html",doc.Html())
+    #DebugSave("a.html",doc.Html())
     elems = doc.Select("div.box > div.group.left-box > div.webtoon-bbs-list.bbs-list > ul > li");
-    DebugPrint(elems.Html());
+    #DebugPrint(elems.Html());
         
     print(doc.Title)
     new_dir = Path.Combine(baseDir, doc.Title.replace(":","_"))
@@ -44,7 +44,7 @@ def getWolfCom(comicsUrl,baseUrl,baseDir):
         url = baseUrl + url
 
         doc = NSoup.NSoupClient.Connect(url).Get()
-        imgs = doc.Select("section.webtoon-body > div.group.image-view > img");
+        imgs = doc.Select("section.webtoon-body > div.group.image-view > img")
         print(doc.Title)
 
         sub_dir = Path.Combine(new_dir, doc.Title.replace(":","_"))
@@ -65,15 +65,52 @@ def getWolfCom(comicsUrl,baseUrl,baseDir):
 def getFoxComics(comicsUrl,baseUrl,baseDir):
     getWolfCom(comicsUrl,baseUrl,baseDir)
 
+def getWToon(comicsUrl,baseUrl,baseDir):
+    doc = NSoup.NSoupClient.Connect(comicsUrl).Get()
+    #DebugSave("a.html",doc.Html())
+    elems = doc.Select("div.row > div.wvlist > div.left-box > div.wbody > ul > li");
+    #DebugPrint(elems.Html());
+        
+    print(doc.Title)
+    new_dir = Path.Combine(baseDir, doc.Title.replace(":","_"))
+    if not Directory.Exists(new_dir): Directory.CreateDirectory(new_dir)
+    print(new_dir)
+   
+    for e in elems:
+        url = e.Select("a").First.Attr("href")
+        if not url: continue
+        url = baseUrl + url
+
+        doc = NSoup.NSoupClient.Connect(url).Get()
+        imgs = doc.Select("section.body > div.container > div.row > div.wview > div.wbody > img")
+        print(doc.Title)
+        #DebugSave("b.html",doc.Html())
+        
+        sub_dir = Path.Combine(new_dir, doc.Title.replace(":","_"))
+        if not Directory.Exists(sub_dir): Directory.CreateDirectory(sub_dir)
+
+        k = 1;
+        for img in imgs:
+            img_url = img.Attr('src')
+            if not img_url: continue
+            if image_ext == None or img_url.endswith(image_ext):
+                if( not img_url.startswith("http") ):
+                    img_url = baseUrl + img_url
+                file_name = "img_%04d.jpg" % k
+                Download( img_url, Path.Combine(sub_dir, file_name), comicsUrl)
+                print( img_url + " -> " + file_name )
+                k = k + 1
+
 if __name__ == "__main__":
     url = [ 
-        "https://fxfx21.com/list?toon=1235&title=%B8%F0%B5%CE%C0%C7%BE%CB%B9%D9",
+        "https://wtwt30.com/v1?toon=1079",
     ]
     baseDir = r"D:\Temp2"
     for u in url:
         pos = u.find("/",len('https://')) #"https://wfwf105.com",https://fxfx21.com
         baseUrl = u[:pos+1]
-        getFoxComics(u,baseUrl,baseDir)
+        #getFoxComics(u,baseUrl,baseDir)
+        getWToon(u,baseUrl,baseDir)
     
     
     
